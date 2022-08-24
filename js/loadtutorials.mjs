0c8595e2a79194b32baeae77027f26dfc682338b
Marvin <marvin.rosskothen@outlook.de>
2022-08-24 13:21:47 +0200

    Initial commit

---
+++ js/loadtutorials.mjs
@@ -0,0 +1,61 @@
+function loadTutorials(clear, search = null) {
+    addLoading(true);
+    const container = document.querySelector('.tutorials');
+
+    if (clear) {
+        container.innerHTML = "";
+        if (document.querySelector('.end') && document.querySelector('.end') != null) {
+            document.querySelector('.end').remove();
+        }
+    }
+
+    fetch("tutorials.json")
+        .then(response => {
+            return response.json();
+        })
+        .then(json => {
+            for (let i = 0; i < json.length; i++) {
+                console.log(json[i]);
+
+                const card = document.createElement('div');
+                card.classList.add("tutorial-card");
+
+                const title = document.createElement('h2');
+                title.classList.add("title");
+                title.textContent = json[i].title;
+
+                const description = document.createElement('p');
+                description.classList.add("description");
+                let dscTxt = json[i].description;
+                if (dscTxt.length > 90) {
+                    description.title = dscTxt;
+                    dscTxt = dscTxt.substring(0, 90) + "...";
+                }
+                description.textContent = dscTxt;
+
+                const tagsCont = document.createElement('div');
+                tagsCont.classList.add("tags");
+                json[i].tags.forEach(tag => {
+                    tagsCont.innerHTML += `<span class='tag' onclick='filterTag("${tag}");'>${tag}</span>`;
+                });
+
+                const button = document.createElement('a');
+                button.classList.add("button");
+                button.href = json[i].file;
+                button.textContent = "Tutorial anzeigen";
+
+                card.appendChild(title);
+                card.appendChild(description);
+                card.appendChild(tagsCont);
+                card.appendChild(button);
+
+                if (search === null || search === "" || json[i].tags.includes(search)) {
+                    container.appendChild(card);
+                }
+            }
+
+            document.querySelector('main').innerHTML += "<span class='end'>Ende der Liste.</span>";
+        });
+
+    removeLoading();
+}
---
+++ js/main.js
@@ -0,0 +1,46 @@
+$(function () {
+    addLoading(true);
+});
+
+function addLoading(active = false) {
+    if (document.querySelector('.loading-wrapper') == null) {
+        const loadingWrapper = document.createElement('div');
+        loadingWrapper.classList.add('loading-wrapper');
+        if (active) {
+            loadingWrapper.classList.add('show');
+        }
+        loadingWrapper.innerHTML = "<div class='loading-animation-wrapper'><div></div><div></div><div></div></div>";
+        document.body.appendChild(loadingWrapper);
+    }
+}
+
+function removeLoading() {
+    var animation = {opacity: 0};
+    $('.loading-wrapper').animate(animation, 'slow', 'swing', function () {
+        setTimeout(() => {
+            this.remove();
+        }, 500);
+    });
+}
+
+function filterTag(tag) {
+    console.log("Du möchtest suchen nach: " + tag);
+    loadTutorials(true, tag);
+}
+
+function initSlideIn(selector) {
+    $(function () {
+        $(selector).css("translateY", "25px");
+        $(selector).css("opacity", "0");
+
+        $(selector).waypoint(function () {
+            var CSStransforms = anime({
+                targets: this.element,
+                translateY: 0,
+                opacity: 1
+            });
+        }, {
+            offset: '100%'
+        });
+    });
+}
