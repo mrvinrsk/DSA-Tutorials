751927fb5abf6c43395669c80546836c71bd634d
Marvin <marvin.rosskothen@outlook.de>
2022-09-26 16:51:06 +0200

    added load- / savesystem for checklists

--- js/checklist.js
+++ js/checklist.js
@@ -26,26 +26,137 @@ function getProgress(checklist) {
     return string;
 }
 
-function updateList(checklist) {
-    const progress = (checklist.querySelector('.progress') ? checklist.querySelector('.progress') : document.createElement('span'));
-    progress.classList.add('progress');
-    progress.innerHTML = `${getProgress(checklist)}`;
+function readText(filePath) {
+    let output = ""; //placeholder for text output
+    if (filePath.files && filePath.files[0]) {
+        reader.onload = function (e) {
+            output = e.target.result;
+            return output;
+        };//end onload()
+        reader.readAsText(filePath.files[0]);
+    }//end if html5 filelist support
+    else if (ActiveXObject && filePath) { //fallback to IE 6-8 support via ActiveX
+        try {
+            reader = new ActiveXObject("Scripting.FileSystemObject");
+            let file = reader.OpenTextFile(filePath, 1); //ActiveX File Object
+            output = file.ReadAll(); //text contents of file
+            file.Close(); //close file "input stream"
+            return output;
+        } catch (e) {
+            if (e.number === -2146827859) {
+                alert('Unable to access local files due to browser security settings. ' +
+                    'To overcome this, go to Tools->Internet Options->Security->Custom Level. ' +
+                    'Find the setting for "Initialize and script ActiveX controls not marked as safe" and change it to "Enable" or "Prompt"');
+            }
+        }
+    } else { //this is where you could fallback to Java Applet, Flash or similar
+        return false;
+    }
+}
 
-    if (finished(checklist)) {
-        progress.classList.add('finished');
-        checklist.querySelector('.checks').classList.add('finished');
-    } else {
-        progress.classList.remove('finished');
-        checklist.querySelector('.checks').classList.remove('finished');
+
+async function getFileContent(evt) {
+    //Retrieve the first (and only!) File from the FileList object
+    var myFile = evt.target.files[0];
+    var reader = new FileReader();
+    reader.readAsText(myFile);
+    reader.onload = function () {
+        let result = reader.result;
+    }
+}
+
+function importProgress(checklist) {
+    let input = document.createElement('input');
+    input.type = 'file';
+    input.accept = '.clp,.txt';
+    input.click();
+
+    input.onchange = e => {
+        let file = e.target.files[0];
+
+        var reader = new FileReader();
+        reader.readAsText(file);
+        reader.onload = function () {
+            let result = reader.result;
+            let ids = result.split(';');
+
+            checklist.querySelectorAll('input[type=checkbox]').forEach(checkbox => {
+                if (ids.includes(checkbox.id)) {
+                    checkbox.checked = true;
+                }else {
+                    checkbox.checked = false;
+                }
+            });
+
+            updateList(checklist);
+        }
+    }
+}
+
+function exportProgress(checklist) {
+    let checked = checklist.querySelectorAll('input[type=checkbox]:checked');
+    let ids = [];
+
+    checked.forEach(check => {
+        ids.push(check.id);
+    });
+
+    let str = ids.join(';');
+    let fname = (checklist.hasAttribute('data-name') ? checklist.dataset.name : checklist.id);
+    let date = new Date();
+    let ts = [date.getDate(), (date.getMonth() + 1), date.getFullYear()].join('_');
+
+    download(`${fname}-${ts}.txt`, str);
+}
+
+function updateList(checklist) {
+    if(checklist.hasAttribute('data-show-completion')) {
+        const progress = (checklist.querySelector('.progress') ? checklist.querySelector('.progress') : document.createElement('span'));
+        progress.classList.add('progress');
+        progress.innerHTML = `${getProgress(checklist)}`;
+
+        if (finished(checklist)) {
+            progress.classList.add('finished');
+        }else {
+            progress.classList.remove('finished');
+        }
+
+        const firstCheck = checklist.firstChild;
+        checklist.insertBefore(progress, firstCheck);
     }
 
-    const firstCheck = checklist.firstChild;
-    checklist.insertBefore(progress, firstCheck);
+    if (finished(checklist)) {
+        checklist.querySelector('.checks').classList.add('finished');
+    } else {
+        checklist.querySelector('.checks').classList.remove('finished');
+    }
 }
 
 $(function () {
     document.querySelectorAll('.checklist').forEach(checklist => {
+        let cb_i = 0;
         checklist.id = 'checklist-' + (++index);
+        checklist.innerHTML += "<div class='dl-list'></div>";
+
+        checklist.querySelectorAll('input[type=checkbox]').forEach(checkbox => {
+            checkbox.id = `check-${(++cb_i)}`;
+        });
+
+        const imp = document.createElement('a');
+        imp.textContent = "Laden";
+        imp.classList.add('button', 'small');
+        imp.addEventListener('click', () => {
+            importProgress(checklist);
+        });
+
+        const exp = document.createElement('a');
+        exp.textContent = "Speichern";
+        exp.classList.add('button', 'small', 'speichern');
+        exp.addEventListener('click', () => {
+            exportProgress(checklist);
+        });
+
+        checklist.querySelector('.dl-list').append(imp, exp);
 
         if (checklist.hasAttribute('data-show-completion')) {
             updateList(checklist);
--- js/main.js
+++ js/main.js
@@ -20,6 +20,19 @@ function isInView(element) {
     );
 }
 
+function download(filename, text) {
+    let element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+}
+
 function removeLoading() {
     var animation = {opacity: 0};
     $('.loading-wrapper').animate(animation, 'slow', 'swing', function () {
